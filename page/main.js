0f7d88b2b31bf164ce8c7c5eb4be04103ad36f6e
Bruno Marques <116980642+brunomarques97@users.noreply.github.com>
2024-01-08 04:32:38 -0300

    Criação da aplicação First mobile

---
+++ page/main.js
@@ -0,0 +1,55 @@
+function pergunta1() {
+    let botaoplus = document.getElementById("pergunta1plus")
+    let botaominus = document.getElementById("pergunta1minus")
+    if(botaoplus.hidden == false){
+        document.getElementById("pergunta1").hidden = false;
+        botaominus.hidden = false;
+        botaoplus.hidden = true;
+    }else if(botaominus.hidden == false){
+        document.getElementById("pergunta1").hidden = true;
+        botaominus.hidden = true;
+        botaoplus.hidden = false;
+    }
+  }
+
+  function pergunta2() {
+    let botaoplus = document.getElementById("pergunta2plus")
+    let botaominus = document.getElementById("pergunta2minus")
+    if(botaoplus.hidden == false){
+        document.getElementById("pergunta2").hidden = false;
+        botaominus.hidden = false;
+        botaoplus.hidden = true;
+    }else if(botaominus.hidden == false){
+        document.getElementById("pergunta2").hidden = true;
+        botaominus.hidden = true;
+        botaoplus.hidden = false;
+    }
+  }
+
+  function pergunta3() {
+    let botaoplus = document.getElementById("pergunta3plus")
+    let botaominus = document.getElementById("pergunta3minus")
+    if(botaoplus.hidden == false){
+        document.getElementById("pergunta3").hidden = false;
+        botaominus.hidden = false;
+        botaoplus.hidden = true;
+    }else if(botaominus.hidden == false){
+        document.getElementById("pergunta3").hidden = true;
+        botaominus.hidden = true;
+        botaoplus.hidden = false;
+    }
+  }
+
+  function pergunta4() {
+    let botaoplus = document.getElementById("pergunta4plus");
+    let botaominus = document.getElementById("pergunta4minus");
+    if(botaoplus.hidden == false){
+        document.getElementById("pergunta4").hidden = false;
+        botaominus.hidden = false;
+        botaoplus.hidden = true;
+    }else if(botaominus.hidden == false){
+        document.getElementById("pergunta4").hidden = true;
+        botaominus.hidden = true;
+        botaoplus.hidden = false;
+    }
+  }
